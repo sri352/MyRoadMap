07c2cef974c2eea7031cac4dbb23dfb2e3c67efd
JustDoIt <sriram.vuppuluri@gmail.com>
2020-10-01 09:48:48 +0530

    Added basic info messages.

---
+++ CompetitiveCoding/Java/SearchAlgorithms/LinearSearch.java
@@ -0,0 +1,10 @@
+public private static int linearSearch(int[] arr, int x) {
+    
+    //Basic linearSearch which returns the first index of the found element else -1;
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]==x){
+            return i;
+    }
+    return -1;
+
+}
