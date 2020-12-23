3614aa0d2192469307ff4d8b96fe663b296bd4fd
sri352 <sriram.vuppuluri@gmail.com>
2020-12-24 00:23:36 +0530

    basic mistakes

--- CompetitiveCoding/Java/SearchAlgorithms/LinearSearch.java
+++ CompetitiveCoding/Java/SearchAlgorithms/LinearSearch.java
@@ -4,7 +4,8 @@ public static int linearSearch(int[] arr, int x) {
     for(int i=0;i<arr.length;i++){
         if(arr[i]==x){
             return i;
+        }
+        return -1;
     }
-    return -1;
 
 }
